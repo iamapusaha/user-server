bc311333851ba3fecb6fa8f6d094634c001091f9
iamapusaha <apusaha392@gmail.com>
2023-10-11 22:39:51 +0600

    server is ready to serve

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const users = require('./users.json');
+console.log(users);
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors())
+app.use(express.json())
 app.get('/', (req, res) => {
     res.send('welcome to your new server')
 })
@@ -13,6 +15,10 @@ app.get('/users', (req, res) => {
 app.post('/users', (req, res) => {
     console.log('post spi hitting');
     console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser)
+    res.send(newUser)
 })
 app.listen(port, () => {
     console.log(`super you server is rummimg and port is ${port}`);
