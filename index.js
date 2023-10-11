439f09263d5d43d50c15c7b12c621a4cd6af98bb
iamapusaha <apusaha392@gmail.com>
2023-10-11 17:20:34 +0600

    use cors middleware

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const users = require('./users.json');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-
+app.use(cors())
 app.get('/', (req, res) => {
     res.send('welcome to your new server')
 })
